23c7565730e17ca9744567fcae87d5d1b0d3626f
Lunarprom <haimanduan@s-164-67-71-136.resnet.ucla.edu>
2016-02-23 23:34:20 -0800

    ItemBean

--- proj4_Haiman/src/edu/ucla/cs/cs144/ItemServlet.java
+++ proj4_Haiman/src/edu/ucla/cs/cs144/ItemServlet.java
@@ -1,5 +1,6 @@
 package edu.ucla.cs.cs144;
 
+import java.util.*;
 import java.io.IOException;
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
@@ -7,12 +8,36 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.w3c.dom.*;
+
+
 public class ItemServlet extends HttpServlet implements Servlet {
        
     public ItemServlet() {}
 
+    public ItemBin parseXMLString(String xml){
+        Document doc = null
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        try {
+        	doc = dBuilder.parse(new InputSource(new StringReader(xml)));
+        } catch (Exception e) {
+        	e.printStackTrace();
+        }
+
+        if (doc == null) return null;
+
+        Element item = doc.getDocumentElement();
+
+        String itemID = item.getAttribute("")
+
+        return itemBin;
+    }
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        // your codes here
+        String itemID = request.getParameter("id");
+        String xml = AuctionSearchClient.getXMLDataForItemId(itemID);
+        ItemBin itemBin = parseXMLString(xml);
     }
 }
