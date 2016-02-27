1ac7bcf1c58a6294fa6c61605b454fcae87b702f
Lunarprom <haimanduan@zhangying.local>
2016-02-26 22:50:29 -0800

    AutoSuggest

--- proj4/WebContents/javaScript/SuggestionProvider.js
+++ proj4/WebContents/javaScript/SuggestionProvider.js
@@ -1,12 +1,31 @@
+function getXmlHttpRequestObject() {
+	if (window.XMLHttpRequest) {
+		return new XMLHttpRequest(); //For Firefox/Safari
+	} else if(window.ActiveXObject) {
+		return new ActiveXObject("Microsoft.XMLHTTP"); //For IE
+	} else {
+		alert("Error!");
+	}
+}
+
+var suggestReq = getXmlHttpRequestObject();
+
+
 //Build a Suggestion Provider
-function SuggestionProvider() {
+function StateSuggestions() {
 	//initializations
 }
 
-SuggestionProvider.prototype.requestSuggestions = function (oAutoSuggestControl) {
-	var aSuggestions = new Array();
+StateSuggestions.prototype.requestSuggestions = function (oAutoSuggestControl, bTypeAhead) {
+	var aSuggestions = [];
+	var sTextboxValue = oAutoSuggestControl.textbox.value;
+
+	if (sTextboxValue.length > 0){
+
+		for (var i = 0; i < this.states.)
+	}
 
 	//determine suggestions for the control
-	oAutoSuggestControl.autosuggest(aSuggestions);
-}
+	oAutoSuggestControl.autosuggest(aSuggestions, bTypeAhead);
+};
 
