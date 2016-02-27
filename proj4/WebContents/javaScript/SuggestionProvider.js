db6a404548d95c16c72672f2f1a5aaabdbe3006e
Lunarprom <haimanduan@s-164-67-71-64.resnet.ucla.edu>
2016-02-27 00:22:11 -0800

    AutoSuggest

--- proj4/WebContents/javaScript/SuggestionProvider.js
+++ proj4/WebContents/javaScript/SuggestionProvider.js
@@ -1,31 +1,44 @@
-function getXmlHttpRequestObject() {
-	if (window.XMLHttpRequest) {
-		return new XMLHttpRequest(); //For Firefox/Safari
-	} else if(window.ActiveXObject) {
-		return new ActiveXObject("Microsoft.XMLHTTP"); //For IE
-	} else {
-		alert("Error!");
-	}
+if (window.XMLHttpRequest) {
+	var xmlHttp = new XMLHttpRequest();
+} else if (window.ActiveXObject) {
+    var xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+} else {
+    alert("Error!");
 }
 
-var suggestReq = getXmlHttpRequestObject();
-
-
-//Build a Suggestion Provider
 function StateSuggestions() {
-	//initializations
 }
 
-StateSuggestions.prototype.requestSuggestions = function (oAutoSuggestControl, bTypeAhead) {
-	var aSuggestions = [];
+StateSuggestions.prototype.requestSuggestions = function (oAutoSuggestControl /*:AutoSuggestControl*/,
+                                                          bTypeAhead /*:boolean*/) {
 	var sTextboxValue = oAutoSuggestControl.textbox.value;
+    var request = "suggest?q=" + encodeURI(sTextboxValue);
 
-	if (sTextboxValue.length > 0){
 
-		for (var i = 0; i < this.states.)
-	}
-
-	//determine suggestions for the control
-	oAutoSuggestControl.autosuggest(aSuggestions, bTypeAhead);
+    xmlHttp.open("GET", request);
+    xmlHttp.onreadystatechange = this.provideSuggestion(oAutoSuggestControl, bTypeAhead);
+    xmlHttp.send(null);
 };
 
+
+StateSuggestions.prototype.provideSuggestion = function (oAutoSuggestControl, bTypeAhead) {
+	return function() {
+		if (XMLHttp.readyState == 4) {
+			
+			var aSuggestions = [];
+			var completeSuggestion = xmlHttp.responseXML.getElementbyTagName("CompleteSuggestion");
+
+			for (var i = 0; i < completeSuggestion.length; ++i) {
+				var text = completeSuggestion[i].childNodes[0].getAttribute("data");
+				aSuggestions.push(text);
+			}
+
+			if (aSuggestions.length> 0) {
+				oAutoSuggestControl.autosuggest(aSuggestions, bTypeAhead);
+			} else {
+				oAutoSuggestControl.hideSuggestions();
+			}
+			
+		}
+	}
+};
